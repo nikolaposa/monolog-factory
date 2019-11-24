efa1dd54186c6b19434be72b948ccbb17262b0fd
Nikola Posa <posa.nikola@gmail.com>
2019-11-24 20:29:07 +0100

    Fix tests

--- tests/TestAsset/Logger/ErroneousHandlerFactory.php
+++ tests/TestAsset/Logger/ErroneousHandlerFactory.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace MonologFactory\Tests\TestAsset\Logger;
 
+use RuntimeException;
+
 class ErroneousHandlerFactory
 {
-
+    public function __construct()
+    {
+        throw new RuntimeException('Intentional error');
+    }
 }
