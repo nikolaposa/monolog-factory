6405b7072c186588d6e8b07a8498359470f81bec
Nikola Poša <posa.nikola@gmail.com>
2026-06-24 09:46:26 +0200

    Update and rename .php_cs to .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,16 +2,14 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->exclude('vendor')
-;
+    ->exclude('vendor');
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
+    ->setFinder($finder)
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
         'declare_strict_types' => true,
         'return_type_declaration' => ['space_before' => 'none'],
         'phpdoc_line_span' => ['property' => 'single'],
-    ])
-    ->setFinder($finder)
-;
+    ]);
